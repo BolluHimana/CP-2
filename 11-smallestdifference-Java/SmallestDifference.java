c3a2840eabb2d4e5dbcfcd5f8b3e49de677a938e
BolluHimana <55687513+BolluHimana@users.noreply.github.com>
2020-07-13 17:00:05 +0530

    Initialize 11-smallestdifference-Java

---
+++ 11-smallestdifference-Java/SmallestDifference.java
@@ -0,0 +1,14 @@
+// Write the function smallestDifference(a) that takes a list of integers and returns 
+// the smallest absolute difference between any two 
+// integers in the list. If the list is empty, return -1. For example:
+//       assert(smallestDifference([19,2,83,6,27]) == 4)
+// The two closest numbers in that list are 2 and 6, and their difference is 4.
+import java.util.*;
+
+public class SmallestDifference {
+
+    public int smallestDifference(int[] a) {
+        // Your code goes here
+        return -1;
+    }
+}
