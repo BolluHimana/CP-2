be2bbc1dd2418cff34af3eb336764a7b13b1bea5
BolluHimana <55687513+BolluHimana@users.noreply.github.com>
2020-07-13 17:01:09 +0530

    Initialize 03-nth_pronicnumber-Java

---
+++ 03-nth_pronicnumber-Java/NthPronicNumber.java
@@ -0,0 +1,11 @@
+// nthPronicNumber(n) [20 pts]
+// Write the function nthPronicNumber that takes a non-negative int n and returns the nth Pronic 
+// Number. Pronic number is a number which is the product of two consecutive integers, that is, a 
+// number n is a product of x and (x+1).
+
+public class NthPronicNumber {
+	public long nthPronicNumber(int n) {
+		// Your code goes here
+		return 1;
+	}
+}
---
+++ 03-nth_pronicnumber-Java/TestJUnit.java
@@ -0,0 +1,70 @@
+/**
+ * This is JUnit that tests the sleepIn method that is available in SleepIn class.
+ * This contains 4 testcases.
+ * 
+ * Please don't run this file.
+ * You can add your own test cases to this file by just copy and paste the last three 
+ * lines of the code (TestCase4).
+ * 
+ * @author: Siva Sankar
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestJUnit {
+   @Test
+   public void testCase1() {
+		assertEquals(new NthPronicNumber().nthPronicNumber(0), 0);
+		assertEquals(new NthPronicNumber().nthPronicNumber(1), 2);
+		assertEquals(new NthPronicNumber().nthPronicNumber(2), 6);
+		assertEquals(new NthPronicNumber().nthPronicNumber(3), 12);
+		assertEquals(new NthPronicNumber().nthPronicNumber(4), 20);
+		assertEquals(new NthPronicNumber().nthPronicNumber(5), 30);
+		assertEquals(new NthPronicNumber().nthPronicNumber(6), 42);
+		assertEquals(new NthPronicNumber().nthPronicNumber(7), 56);
+		assertEquals(new NthPronicNumber().nthPronicNumber(8), 72);
+		assertEquals(new NthPronicNumber().nthPronicNumber(9), 90);
+		assertEquals(new NthPronicNumber().nthPronicNumber(10), 110);
+		assertEquals(new NthPronicNumber().nthPronicNumber(11), 132);
+		assertEquals(new NthPronicNumber().nthPronicNumber(12), 156);
+		assertEquals(new NthPronicNumber().nthPronicNumber(13), 182);
+		assertEquals(new NthPronicNumber().nthPronicNumber(14), 210);
+		assertEquals(new NthPronicNumber().nthPronicNumber(15), 240);
+		assertEquals(new NthPronicNumber().nthPronicNumber(16), 272);
+		assertEquals(new NthPronicNumber().nthPronicNumber(17), 306);
+		assertEquals(new NthPronicNumber().nthPronicNumber(18), 342);
+		assertEquals(new NthPronicNumber().nthPronicNumber(19), 380);
+		assertEquals(new NthPronicNumber().nthPronicNumber(20), 420);
+		assertEquals(new NthPronicNumber().nthPronicNumber(21), 462);
+		assertEquals(new NthPronicNumber().nthPronicNumber(22), 506);
+		assertEquals(new NthPronicNumber().nthPronicNumber(23), 552);
+		assertEquals(new NthPronicNumber().nthPronicNumber(24), 600);
+		assertEquals(new NthPronicNumber().nthPronicNumber(25), 650);
+		assertEquals(new NthPronicNumber().nthPronicNumber(26), 702);
+		assertEquals(new NthPronicNumber().nthPronicNumber(27), 756);
+		assertEquals(new NthPronicNumber().nthPronicNumber(28), 812);
+		assertEquals(new NthPronicNumber().nthPronicNumber(29), 870);
+		assertEquals(new NthPronicNumber().nthPronicNumber(30), 930);
+		assertEquals(new NthPronicNumber().nthPronicNumber(31), 992);
+		assertEquals(new NthPronicNumber().nthPronicNumber(32), 1056);
+		assertEquals(new NthPronicNumber().nthPronicNumber(33), 1122);
+		assertEquals(new NthPronicNumber().nthPronicNumber(34), 1190);
+		assertEquals(new NthPronicNumber().nthPronicNumber(35), 1260);
+		assertEquals(new NthPronicNumber().nthPronicNumber(36), 1332);
+		assertEquals(new NthPronicNumber().nthPronicNumber(37), 1406);
+		assertEquals(new NthPronicNumber().nthPronicNumber(38), 1482);
+		assertEquals(new NthPronicNumber().nthPronicNumber(39), 1560);
+		assertEquals(new NthPronicNumber().nthPronicNumber(40), 1640);
+		assertEquals(new NthPronicNumber().nthPronicNumber(41), 1722);
+		assertEquals(new NthPronicNumber().nthPronicNumber(42), 1806);
+		assertEquals(new NthPronicNumber().nthPronicNumber(43), 1892);
+		assertEquals(new NthPronicNumber().nthPronicNumber(44), 1980);
+		assertEquals(new NthPronicNumber().nthPronicNumber(45), 2070);
+		assertEquals(new NthPronicNumber().nthPronicNumber(46), 2162);
+		assertEquals(new NthPronicNumber().nthPronicNumber(47), 2256);
+		assertEquals(new NthPronicNumber().nthPronicNumber(48), 2352);
+		assertEquals(new NthPronicNumber().nthPronicNumber(49), 2450);
+		assertEquals(new NthPronicNumber().nthPronicNumber(50), 2550);
+   }
+}
