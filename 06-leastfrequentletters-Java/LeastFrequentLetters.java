6452bc13497d3a240d69c05914ca6d586b90cb75
BolluHimana <55687513+BolluHimana@users.noreply.github.com>
2020-07-13 17:01:17 +0530

    Initialize 06-leastfrequentletters-Java

---
+++ 06-leastfrequentletters-Java/LeastFrequentLetters.java
@@ -0,0 +1,15 @@
+// leastFrequentLetters(s) [20 pts]
+// Write the function leastFrequentLetters(s), that takes a string s, and ignoring case (so "A" and "a" are treated 
+// the same), returns a lowercase string containing the least-frequent alphabetic letters that occur in s, each 
+// included only once in the result and then in alphabetic order. So:
+// leastFrequentLetters("aDq efQ? FB'daf!!!") 
+// returns "be". Note that digits, punctuation, and whitespace are not letters! Also note that seeing as we have not 
+// yet covered lists, sets, maps, or efficiency, you are not expected to write the most efficient solution. Finally, 
+// if s does not contain any alphabetic characters, the result should be the empty string ("")
+
+public class LeastFrequentLetters {
+	public String leastFrequentLetters(String s) {
+		// Your code goes here
+		return "";
+	}
+}
