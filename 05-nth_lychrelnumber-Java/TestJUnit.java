b7077ebd02fb10b1d67292c5d07a3943a1ae9f75
BolluHimana <55687513+BolluHimana@users.noreply.github.com>
2020-07-13 17:01:12 +0530

    Initialize 05-nth_lychrelnumber-Java

---
+++ 05-nth_lychrelnumber-Java/TestJUnit.java
@@ -0,0 +1,64 @@
+/**
+ * This is JUnit that tests the sleepIn method that is available in SleepIn class.
+ * This contains 4 testcases.
+ * 
+ * Please don't run this file.
+ * You can add your own test cases to this file by just copy and paste the last three 
+ * lines of the code (TestCase4).
+ * 
+ * @author: Siva Sankar
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestJUnit {
+   @Test
+   public void testCase1() {
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(1), 196);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(2), 295);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(3), 394);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(4), 493);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(5), 592);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(6), 689);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(7), 691);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(8), 788);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(9), 790);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(10), 879);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(11), 887);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(12), 978);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(13), 986);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(14), 1495);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(15), 1497);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(16), 1585);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(17), 1587);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(18), 1675);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(19), 1677);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(20), 1765);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(21), 1767);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(22), 1855);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(23), 1857);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(24), 1945);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(25), 1947);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(26), 1997);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(27), 2494);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(28), 2496);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(29), 2584);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(30), 2586);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(31), 2674);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(32), 2676);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(33), 2764);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(34), 2766);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(35), 2854);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(36), 2856);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(37), 2944);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(38), 2946);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(39), 2996);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(40), 3493);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(41), 3495);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(42), 3583);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(43), 3585);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(44), 3673);
+		assertEquals(new NthLychrelNumber().nthLychrelNumber(45), 3675);
+   }
+}
