d55d43101efa29c86b2d905f008db651b0bd9386
BolluHimana <55687513+BolluHimana@users.noreply.github.com>
2020-07-13 17:00:07 +0530

    Initialize 12-matrixadd-Python

---
+++ 12-matrixadd-Python/test_matrixadd.py
@@ -0,0 +1,17 @@
+
+import os,sys
+sys.path.append(os.getcwd())
+from matrixadd import matrixadd
+import pytest
+
+
+@pytest.mark.parametrize("L, M, N",[
+	([[1,  2,  3],[4,  5,  6]], [[21, 22, 23], [24, 25, 26]], [[23, 24, 26],[28, 30, 32]]),
+	([[1,  2,  3],[4,  5,  6], [7, 8, 9]], [[1,  2,  3],[4,  5,  6], [7, 8, 9]], [[2, 4, 6],[8, 10, 12], [14, 16, 18]]),
+	([[1,  2,  3],[4,  5,  6]], [[21, 22, 23], [24, 25]], None),
+	([[1]], [[10]], [[11]]),
+	([[1, 2]], [[10]], None),
+
+])
+def test_matrixadd(L, M, N):
+    assert matrixadd(L, M) == N
