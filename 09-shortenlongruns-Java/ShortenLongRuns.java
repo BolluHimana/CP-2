6edbfac29d233200a474a67b872ba3f31c6d52bb
BolluHimana <55687513+BolluHimana@users.noreply.github.com>
2020-07-13 17:00:31 +0530

    Initialize 09-shortenlongruns-Java

---
+++ 09-shortenlongruns-Java/ShortenLongRuns.java
@@ -0,0 +1,16 @@
+// shortenLongRuns(L, k) [15 pts]
+// Write the non-destructive function shortenLongRuns(L, k) that takes a list L and a positive integer k, and 
+// non-destructively returns a similar list, only without any run of k consecutive equal values in L. This means that 
+// any values that would otherwise produce a consecutive run of k elements are not present. 
+// For example: 
+// assert(shortenLongRuns([2, 3, 5, 5, 5, 3], 2) == [2, 3, 5, 3]) 
+// assert(shortenLongRuns([2, 3, 5, 5, 5, 3], 3) == [2, 3, 5, 5, 3]) 
+// Note: your function may not just create a copy of L and call the destructive version of this function (below) on 
+// that copy and return it. Instead, you must directly construct the result here.
+
+public class ShortenLongRuns {
+	public int[] shortenLongRuns(int[] arr, int k) {
+		// Your code goes here
+		return new int[0];
+	}
+}
