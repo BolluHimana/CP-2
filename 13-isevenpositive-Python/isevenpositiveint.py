ea099a7ff7d53d300a79299f3db59c4f4b5b1b32
BolluHimana <55687513+BolluHimana@users.noreply.github.com>
2020-07-13 16:59:19 +0530

    Initialize 13-isevenpositive-Python

---
+++ 13-isevenpositive-Python/isevenpositiveint.py
@@ -0,0 +1,9 @@
+# isevenpositiveint(x)
+# Write the function isevenpositiveint(x) that takes an arbitrary value x, return True if it is an
+# integer, and it is positive, and it is even (all 3 must be True), or False otherwise. Do not
+# crashing if the value is not an integer. So, isevenpositiveint("yikes!") returns False (rather
+# than crashing), and isevenpositiveint(123456) returns True.
+
+def isevenpositiveint(x):
+	# your code goes here
+	pass
---
+++ 13-isevenpositive-Python/test_isevenpositiveint.py
@@ -0,0 +1,23 @@
+import pytest
+from isevenpositiveint import isevenpositiveint
+import os
+import sys
+sys.path.append(os.getcwd())
+
+
+@pytest.mark.parametrize('x, check', [
+    (1, False),
+    (-1, False),
+    (-2, False),
+    (-3, False),
+    (2, True),
+    (3, False),
+    (1.0, False),
+    ("yikes!", False),
+    (None, False),
+    ((12), False),
+    ([12], False),
+    (123456, True)
+])
+def test_isevenpositiveint(x, check):
+    assert isevenpositiveint(x) == check
