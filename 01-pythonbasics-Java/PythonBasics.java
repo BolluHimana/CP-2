be772434b5c98bc2834fe60a5bd971591d170ae7
BolluHimana <55687513+BolluHimana@users.noreply.github.com>
2020-07-13 16:59:03 +0530

    Initialize 01-pythonbasics-Java

---
+++ 01-pythonbasics-Java/PythonBasics.java
@@ -0,0 +1,13 @@
+// Write a non static method called "show_excitement" where the string
+// "I am super excited for this course!" is returned exactly
+// 5 times, where each sentence is separated by a single space.
+// Return the string with "return".
+// You can only have the string once in your code.
+// Don't just copy/paste it 5 times into a single variable!
+
+
+public class PythonBasics {
+    public String show_excitement() {
+    	// your code goes here
+	}
+}
