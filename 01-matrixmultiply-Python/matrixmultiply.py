f98ffb60ee75a8e175267656b2cc48cf9c40dde5
BolluHimana <55687513+BolluHimana@users.noreply.github.com>
2020-07-13 17:00:41 +0530

    Initialize 01-matrixmultiply-Python

---
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -0,0 +1,12 @@
+# Write the function matrixMultiply(m1, m2) that takes two 2d lists 
+# (that we will consider to be matrices) and returns a new 2d list that 
+# is the result of multiplying the two matrices. Return None if the 
+# two matrices cannot be multiplied for any reason.
+
+
+def fun_matrixmultiply(m1, m2):
+    return None
+
+
+
+
