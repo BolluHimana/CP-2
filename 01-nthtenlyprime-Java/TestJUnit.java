47201b805f76bd390201a4d36db0692485417fc5
BolluHimana <55687513+BolluHimana@users.noreply.github.com>
2020-07-13 17:01:13 +0530

    Initialize 01-nthtenlyprime-Java

---
+++ 01-nthtenlyprime-Java/TestJUnit.java
@@ -0,0 +1,21 @@
+/**
+ * This is JUnit that tests the sleepIn method that is available in SleepIn class.
+ * This contains 4 testcases.
+ * 
+ * Please don't run this file.
+ * You can add your own test cases to this file by just copy and paste the last three 
+ * lines of the code (TestCase4).
+ * 
+ * @author: Siva Sankar
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestJUnit {
+   @Test
+   public void testCase1() {
+      assertEquals(new PythonBasics().show_excitement(), "I am super excited for this course! I am super excited for this course! I am super excited for this course! I am super excited for this course! I am super excited for this course! ");
+   }
+
+}
