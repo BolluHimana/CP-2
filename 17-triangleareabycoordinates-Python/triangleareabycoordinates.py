2378eba8fe1d8066811cb55efd573b39aeccd9de
BolluHimana <bolluhimana@msitprogram.net>
2020-07-15 11:41:34 +0530

    Jul 15  11:41:34

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -8,7 +8,7 @@ import math
 def trianglearea(dist1, dist2, dist3):
 	s=(dist1+dist2+dist3)/2
 	a=math.sqrt((s)*(s-dist1)*(s-dist2)*(s-dist3))
-	return float(a)
+	return a
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
 	dist1=(math.sqrt(math.pow(x2-x1,2)+math.pow(y2-y1,2)))
 	dist2=(math.sqrt(math.pow(x3-x2,2)+math.pow(y3-y2,2)))
