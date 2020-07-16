751a73db321c4f99e8cae99b4dd58705440d0661
BolluHimana <bolluhimana@msitprogram.net>
2020-07-16 23:59:47 +0530

    Jul 16  23:59:46

--- 07-digitcount-Python/digitcount.py
+++ 07-digitcount-Python/digitcount.py
@@ -4,10 +4,7 @@
 # could do this would be to return len(str(abs(n))), but you cannot do that, since you may not use strings 
 # here! This can be solved with logarithms, but seeing as this is "loops week", you should instead simply 
 # repeatedly remove the ones digit until you cannot.
-
+import math
 def digitcount(n):
 	# your code goes here
-	while(n!=0):
-		n=n//10
-		size=size+1
-	return size
+    return math.floor(math.log(n, 10)+1) 
