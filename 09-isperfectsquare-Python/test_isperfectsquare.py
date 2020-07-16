ff7049de788408e30df3e94919d36f88f5b2dc3e
BolluHimana <bolluhimana@msitprogram.net>
2020-07-16 18:44:32 +0530

    Jul 16  18:44:32

--- 09-isperfectsquare-Python/test_isperfectsquare.py
+++ 09-isperfectsquare-Python/test_isperfectsquare.py
@@ -4,14 +4,14 @@ from isperfectsquare import isperfectsquare
 import pytest
 
 
-@pytest.mark.parametrize('n', 'result',[
+@pytest.mark.parametrize('n, result',[
 	("hello", False), 
 	(6.25, False), 
 	(625, True), 
 	(-625, False), 
 	(4, True), 
 	(100, True), 
-	("100", True), 
+	("100", False), 
 ])
 def test_isperfectsquare(n, result):
     assert isperfectsquare(n) == result
