4d5e9fc61e0930b4519fb7bbc3f8d8499485a58c
BolluHimana <bolluhimana@msitprogram.net>
2020-07-16 15:12:33 +0530

    Jul 16  15:12:32

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -5,35 +5,29 @@
 # so the 0th digit is the rightmost digit. 
 
 def fun_set_kth_digit(n, k, d):
-	dig = list(str(n))
+	dig = str(n)
+	dChar = str(d)
 	# print (split(nString))
-	# nChar = [c for c in dig]
-	# # print (nChar)
-	# nReverse = list(reversed(nChar))
-	# # print (nReverse)
-	# if(k>=len(dig)):
-	# 	nReverse=nReverse+d
-	# ele=nReverse[k]
-	# newRev = list(reversed(nReverse))
-	if(n<0):
-		if k<len(dig):
-			dig[len(dig)-1-k]=str(d)
-			dig.insert(0,"-")
-			return int("".join(dig))
-		else:
-			dig.insert(0,str(d))
-			dig.insert(0,"-")
-			return int("".join(dig))
+	nChar = [c for c in dig]
+	# print (nChar)
+	nReverse = reversed(nChar)
+	# print (nReverse)
+	if(k>=len(dig)):
+		nReverse=nReverse+dChar
+	ele=nReverse[k]
+	newRev = reversed(nReverse)
+	s=""
+	if(n<0) :
+		s=s+"-"
+		for i in range(len(dChar)):
+			if(dChar[i]!=ele):
+				s=s+dChar[i]
+			if( dChar[i]==ele):
+				s=s+d	
 	else:
-		if k<len(dig):
-			dig[len(dig)-1-k]=str(d)
-			return int("".join(dig))
-		else:
-			dig.insert(0,str(d))
-			return int("".join(dig))
-
-
-
-
-			
-
+		for i in range(len(dChar)):
+			if(dChar[i]!=ele):
+				s=s+dChar[i]
+			if( dChar[i]==ele):
+				s=s+d
+	
