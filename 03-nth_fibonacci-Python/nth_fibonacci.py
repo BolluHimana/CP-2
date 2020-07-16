76bcc8de0a00d9895a33af9f8002694ac48a6a37
BolluHimana <bolluhimana@msitprogram.net>
2020-07-16 22:27:47 +0530

    Jul 16  22:27:46

--- 03-nth_fibonacci-Python/nth_fibonacci.py
+++ 03-nth_fibonacci-Python/nth_fibonacci.py
@@ -8,5 +8,10 @@
 
 
 def fun_nthfibonaccinumber(n):
-	# your code goes Here
-	return 0
+	if(n==0 or n==1):
+		return 1
+	elif(n==2):
+		return 2
+	elif(n>2):
+		return fun_nthfibonaccinumber(n-1)+fun_nthfibonaccinumber(n-2)
+	
