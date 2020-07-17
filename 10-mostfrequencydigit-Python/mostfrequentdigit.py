1fa2fea910078f081d3acd70cff6a57d366889de
BolluHimana <bolluhimana@msitprogram.net>
2020-07-17 11:00:36 +0530

    Jul 17  11:00:36

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -3,13 +3,16 @@
 # that occurs most frequently in it, with ties going to the smaller digit.
 
 def mostfrequentdigit(n):
-	n=str(n)
-	li=[int(i) for i in n]
+	li=[]
+	a=0
+	while(n):
+		a=n%10
+		li.append(a)
+		n=n//10
 	c=[]
 	for  i in range(0,9):
-		list.count(i)
-		c.append(i)
+		m=li.count(i)
+		c.append(m)
 	a=max(c)
 	b=c.index(a)
-	d=li[b]
-	return d
+	d=li
