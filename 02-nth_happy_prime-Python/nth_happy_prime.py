4f222b0c012021d3c9af2a35a1b205af2514917c
BolluHimana <bolluhimana@msitprogram.net>
2020-07-22 00:44:35 +0530

    Jul 22  00:44:35

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -2,7 +2,35 @@
 # Write the function nthHappyPrime(n) which takes a non-negative integer 
 # and returns the nth happy prime number (where the 0th happy prime number is 7).
 
-
-
+def find(n):
+	sum=0
+	while(n>0):
+		a=n%10
+		sum=sum+(a)*(a)
+		n=n//10
+	return sum
+def ishappynumber(n):
+	if(n<=0):
+		return False
+	sum=n
+	while(sum!=1 and sum!=4):
+		sum=find(sum)
+	if(sum==1):
+		return True
+	else:
+		return False
+def isprime(n):
+	if(n>1):
+		for i in range(2,n):
+			if((n%i)!=0):
+				return True
+			else:
+				return False
+				
 def fun_nth_happy_prime(n):
-	return 0
+	l=[]
+	for i in range(100):
+		if(ishappynumber(i)):
+			if(isprime(i)):
+				l.append(i)
+	return l[n]
