bfa0c523194b9a067132d385a7144089de8cb77d
BolluHimana <bolluhimana@msitprogram.net>
2020-07-22 00:58:51 +0530

    Jul 22  00:58:51

--- 02-hasnoprimes-Python/hasnoprimes.py
+++ 02-hasnoprimes-Python/hasnoprimes.py
@@ -3,5 +3,21 @@
 
 
 def fun_hasnoprimes(l):
+
+	for i in range(len(l)):
+		for j in range(len(l[i])):
+			
+			n=l[i][j]
+			m=1
+			c=0
+			while(n>=m):
+				if n%m==0:
+					c=c+1
+					m=m+1
+				else:
+					m=m+1
+			if(c<=2):
+				return False
+				
 	return True
 
