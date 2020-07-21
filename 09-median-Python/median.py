cf7bad70ee0ffdedbda1c65fafbe529323c6ffd6
BolluHimana <bolluhimana@msitprogram.net>
2020-07-21 22:44:54 +0530

    Jul 21  22:44:54

--- 09-median-Python/median.py
+++ 09-median-Python/median.py
@@ -7,7 +7,8 @@ def median(a):
 	lenghth=len(a)
 	if(lenghth==0):
 		return None
-	if(lenghth%2==0):
+		
+	if(lenghth%2==1):
 		return a[lenghth//2]
 	else:
 		return (a[lenghth//2]+a[(lenghth//2)-1])/2
