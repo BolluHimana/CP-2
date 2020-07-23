a5c170d35ddb8de06633ce2084810362841b0eba
BolluHimana <bolluhimana@msitprogram.net>
2020-07-23 19:27:21 +0530

    Jul 23  19:27:20

--- 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
+++ 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
@@ -5,8 +5,8 @@
 # L, return a new list M such that M is the same as L, only with exactly one value changed, and M is a mostly magic 
 # square.
 
-
 def fixmostlymagicsquare(L):
+	
 	l=[]
 	a=[]
 	p=0
@@ -18,21 +18,25 @@ def fixmostlymagicsquare(L):
 		for j in i:
 			s=s+j
 		l.append(s)
+
 	for i in range(len(l)):
 		if(l.count(l[i])==1):
 			p=i
 			result1=l[i]
 		if(l.count(l[i])>1):
 			result=l[i]
+
 	for i in range(len(L)):
 		s=0
 		for j in range(len(L)):
 			s=s+L[j][i]
 		a.append(s)
+
 	for i in range(len(a)):
 		if(a.count(a[i])==1):
 			q=i
 	final=result1-result
+	
 	if(result>0):
 		L[p][q]=L[p][q]-final
 	else:
