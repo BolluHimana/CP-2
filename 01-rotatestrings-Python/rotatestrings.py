ff767e2d1eab35c298941fafe97ad8b9f0085da1
BolluHimana <bolluhimana@msitprogram.net>
2020-07-24 14:26:43 +0530

    Jul 24  14:26:43

--- 01-rotatestrings-Python/rotatestrings.py
+++ 01-rotatestrings-Python/rotatestrings.py
@@ -7,5 +7,17 @@
 
 
 def fun_rotatestrings(s, n):
+	s1=""
+	d=len(s)
+	if n<0:
+		n1=abs(n)
+		n1=n1%d
+		s1=s[d-n1:]+s[:d-n1]
+		return s1
+	if n>0:
+		n=n%d
+		s1=s[n:]+s[:n]
+		return s1
+
 	return s
 
