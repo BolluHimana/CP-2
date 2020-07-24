e464b8d42d91537e85600187080fd35eb7e78c7c
BolluHimana <bolluhimana@msitprogram.net>
2020-07-24 13:21:29 +0530

    Jul 24  13:21:28

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -2,5 +2,18 @@
 Input a list.
 Output a sorted list."""
 def quicksort(array):
-	# Your code goes here
-	pass
+	a=[]
+	b=[]
+	c=[]
+	if len(array)>1:
+		p=array[0]
+		for i in array:
+			if i>p:
+				c.append(i)
+			elif i<p:
+				a.append(i)
+			else:
+				b.append(i)
+		return sorted(c)+b+sorted(a)
+	else:
+		return array
