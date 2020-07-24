1b83952ae65e1d5a0f1a957d14174bf3a688bd54
BolluHimana <bolluhimana@msitprogram.net>
2020-07-24 15:53:06 +0530

    Jul 24  15:53:06

--- 06-lookandsay-Python/lookandsay.py
+++ 06-lookandsay-Python/lookandsay.py
@@ -8,19 +8,20 @@
 # lookAndSay([3,3,8,3,3,3,3]) == [(2,3),(1,8),(4,3)]
 
 def lookandsay(a):
-	if a==[]:
-		return []
-	r=[]
-	k=0
-	n=a[0]
-	for i in range (len(a)):
-		if i==len(a)-1:
-			l1=len(a[k:])
-			r=r+[(l1,n)]
-		if a[i]!=n:
-			l1=len(a[k:i])
-			r=r+[(l1,n)]
-			n=a[i]
-			k=i
-	return  r
-	
+	if a!=[]:
+		r=[]
+		k=0
+		n=a[0]
+		for i in range (len(a)):
+			
+			if a[i]!=n:
+				l1=len(a[k:i])
+				r=r+[(l1,n)]
+				n=a[i]
+				k=i
+			if i==len(a)-1:
+				l1=len(a[k:])
+				r=r+[(l1,n)]
+			
+		return  r
+	return []
