e7a3901fe8fba62c72dc1a3fdf58c4cb64611551
BolluHimana <bolluhimana@msitprogram.net>
2020-07-24 13:53:14 +0530

    Jul 24  13:53:14

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -9,14 +9,35 @@ class BST(object):
         self.root = Node(root)
 
     def insert(self, new_val):
-        # Your code goes here
-        pass
+        if self.root.value==new_val:
+            return False
+        elif new_val<self.root.value:
+            if self.root.left:
+                return self.root.left.insert(new_val)
+            else:
+                self.root.left=Node(new_val)
+                return True
+        else:
+            if self.root.right:
+                return self.root.right.insert(new_val)
+            else:
+                self.root.right=Node(new_val)
+                return True
 
     def printSelf(self):
         # Your code goes here
         pass
         
     def search(self, find_val):
-        # Your code goes here
-        pass
-
+        if find_val==self.root.value:
+            return True
+        elif find_val<self.root.value:
+            if self.root.left:
+                return self.root.left.search(find_val)
+            else:
+                return False
+        else:
+            if self.root.right:
+                return self.root.right.search(find_value)
+            else:
+                return False
