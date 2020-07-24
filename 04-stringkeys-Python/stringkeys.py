3d76939d9c68632154c35998fa44b0b2b618af83
BolluHimana <bolluhimana@msitprogram.net>
2020-07-24 13:32:04 +0530

    Jul 24  13:32:04

--- 04-stringkeys-Python/stringkeys.py
+++ 04-stringkeys-Python/stringkeys.py
@@ -3,6 +3,7 @@ in a hash table, where keys are calculated
 using the first two letters of the string."""
 
 class HashTable(object):
+    
     def __init__(self):
         self.table = [None]*10000
 
