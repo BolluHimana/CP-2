93941e97eb148328da4976a4b85977b79bbe486a
BolluHimana <bolluhimana@msitprogram.net>
2020-07-24 14:57:39 +0530

    Jul 24  14:57:38

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -5,6 +5,18 @@
 
 
 def fun_kth_occurrences(s, n):
-	return 'a'
+	dict1={}
+	c=0
+	for i in s:
+		if i not in dict1.keys():
+			dict1[i]=1
+		else:
+			dict1[i]=dict1[i]+1
+	dict2={k:v for k,v in sorted(dict1.items(),key=lambda item:item[1])}
+	l=[{dict1[k]:k}for k in dict1]
+	l.reverse()
+	a=l[n-1].values()
+	r=l(a)[0]
+	return r
 
 
