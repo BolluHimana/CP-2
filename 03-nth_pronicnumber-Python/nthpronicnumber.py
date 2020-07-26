efca5ce197bb22730ffeccb067c10a820c86bfac
BolluHimana <bolluhimana@msitprogram.net>
2020-07-26 12:17:58 +0530

    Jul 26  12:17:57

--- 03-nth_pronicnumber-Python/nthpronicnumber.py
+++ 03-nth_pronicnumber-Python/nthpronicnumber.py
@@ -6,9 +6,9 @@ import math
 def pronic(n):
 	a=0
 	while int(math.sqrt(n)) >=a:
-		if n!=a*(a+1):
-			a=a+1
-		return True
+		if n==a*(a+1):
+			return True
+		a=a+1
 	return False
 def nthpronicnumber(n):
 	l=[]
