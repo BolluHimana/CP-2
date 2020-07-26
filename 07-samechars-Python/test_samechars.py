ac2c79cf6b79c8ca1b6a6d173d3b729dfec2817e
BolluHimana <bolluhimana@msitprogram.net>
2020-07-26 19:13:49 +0530

    Jul 26  19:13:49

--- 07-samechars-Python/test_samechars.py
+++ 07-samechars-Python/test_samechars.py
@@ -4,12 +4,12 @@ from samechars import samechars
 import pytest
 
 
-@pytest.mark.parametrize('x, result',[
-	(("abcabcabc", "cba") , True),
-	(("abcabcabc", "cbad") , False),
-	(("abcabcabc", "cBa") , False),
-	((42,"The other parameter is not a string") , False),
-	(("","") , True),
+@pytest.mark.parametrize('x,y,result',[
+	("abcabcabc", "cba" , True),
+	("abcabcabc", "cbad", False),
+	("abcabcabc", "cBa" , False),
+	(42,"The other parameter is not a string", False),
+	("","" , True),
 ])
 
 def test_samechars(x, result):
