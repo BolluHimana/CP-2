8315e12716b38b0ddc6a64965ef362b52936ccb9
BolluHimana <bolluhimana@msitprogram.net>
2020-07-26 19:51:01 +0530

    Jul 26  19:51:00

--- 05-longestcommonsubstring-Python/longestcommonsubstring.py
+++ 05-longestcommonsubstring-Python/longestcommonsubstring.py
@@ -8,5 +8,15 @@
 # longestCommonSubstring("abcABC", "zzabZZAB") returns "AB" and not "ab"
 
 def longestcommonsubstring(s1, s2):
-    # Yourcode goes here
-    pass
+    l1=len(s1)
+    new=""
+    for i in range(l1):
+        for j in range(i+1,l1+1):
+            s3=s1[i:j]
+            if s3 in s2:
+                if len(s3)==len(new):
+                    if s3<new:
+                        new=s3
+                elif len(s3)>len(new):
+                    new=s3
+    return new
